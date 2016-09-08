57ea6c74261510acce43cb5918f8d492adf01755
unknown <>
2016-09-08 21:08:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/database/Words.java
@@ -0,0 +1,21 @@
+package com.example.database;
+
+import android.provider.BaseColumns;
+import android.provider.SearchRecentSuggestions;
+
+/**
+ * Created by 杨浩 on 2016/9/2.
+ */
+public class Words {
+    public Words(){
+
+    }
+    public static abstract class Word implements BaseColumns{
+        public static final String Table_name="words";
+        public static final String Column_name_word="word";
+        public static final String Column_name_meaning="meaning";
+        public static final String Column_name_sample="sample";
+
+    }
+
+}
